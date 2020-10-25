0306d4527db51e9289c89025f2839a3b48622cc5
Alexander Graus <alegraus31@gmail.com>
2020-10-25 18:42:47 -0300

    agregada las rutas a citas y contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CitaComponent } from './cita/cita.component';
+import { CitasComponent } from './citas/citas.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:CitasComponent},
+  {path:"cita/:id",component:CitaComponent},
+  {path:"contacto",component:ContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
