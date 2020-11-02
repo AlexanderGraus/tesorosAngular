ea44d116fd3895215600f7b5930a277b775c045f
Alexander Graus <alegraus31@gmail.com>
2020-11-02 16:22:39 -0300

    aniadido llamado al servicio de citas

--- src/app/citas/citas.component.ts
+++ src/app/citas/citas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CitasService } from '../services/citas.service';
 
 @Component({
   selector: 'app-citas',
@@ -6,30 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./citas.component.css']
 })
 export class CitasComponent implements OnInit {
-  citas = [
-    {
-      id:1,
-      autor: "Nicole Graus",
-      libro: "",
-      cita: "O sos careta o sos antisocial, vos elegís"
+  citas = [];
+  constructor(private citasServ:CitasService) {
+    this.citasServ.getAll()
+    .subscribe(data=>{
+      this.citas = data['docs'];
     },
-    {
-      id:2,
-      autor: "Friedrich Nietzsche",
-      libro: "Sobre verdad y mentira en sentido extramoral",
-      cita: "¿Qué es entonces la verdad? Una hueste en movimiento de metáforas, metonimias, antropomorfismos, en resumidas cuentas, una suma de relaciones humanas que han sido realzadas, extrapoladas y adornadas poética y retóricamente y que, después de un prolongado uso, un pueblo considera firmes, canónicas y vinculantes; las verdades son ilusiones de las que se ha olvidado que lo son; metáforas que se han vuelto gastadas y sin fuerza sensible, monedas que han perdido su troquelado y no son ahora ya consideradas como monedas sino como metal"
-    },
-    {
-      id:3,
-      autor: "Canserbero",
-      libro: "Album Muerte, cancion El Primer Trago",
-      cita: ["Que alguien me diga: ¿cómo coño hago?",
-      "Y me encargo de aceptar esta realidad tan bastarda",
-      "Que alguien me diga quien conoce un trago",
-      "Más amargo que tragar un nudo atado a tu garganta"]
+    error=>{
+      console.log('Error:',error);
     }
-  ];
-  constructor() { }
+    );
+   }
 
   ngOnInit(): void {
   }
