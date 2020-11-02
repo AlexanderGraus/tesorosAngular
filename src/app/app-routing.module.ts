a34c77de101c4499a926289ae15283cdb3ba1890
Alexander Graus <alegraus31@gmail.com>
2020-11-02 16:20:41 -0300

    aniadida la ruta contacto y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CitaComponent } from './cita/cita.component';
 import { CitasComponent } from './citas/citas.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
   {path:"",component:CitasComponent},
   {path:"cita/:id",component:CitaComponent},
-  {path:"contacto",component:ContactoComponent}
+  {path:"contacto",component:ContactoComponent},
+  {path:'registro',component:RegistroComponent}
 ];
 
 @NgModule({
