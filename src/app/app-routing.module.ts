d430e25819d237f8b979e46bf055d2fe3e15178a
Alexander Graus <alegraus31@gmail.com>
2020-11-10 17:45:35 -0300

    agregada la ruta login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { CitaComponent } from './cita/cita.component';
 import { CitasComponent } from './citas/citas.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
   {path:"",component:CitasComponent},
   {path:"cita/:id",component:CitaComponent},
   {path:"contacto",component:ContactoComponent},
-  {path:'registro',component:RegistroComponent}
+  {path:'registro',component:RegistroComponent},
+  {path:'login',component:LoginComponent}
 ];
 
 @NgModule({
