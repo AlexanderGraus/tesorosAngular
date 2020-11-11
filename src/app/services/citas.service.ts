3c50a3935c386e468d8bc7c7488454fd662a4302
Alexander Graus <alegraus31@gmail.com>
2020-11-10 22:22:03 -0300

    creacion de funcion getById

--- src/app/services/citas.service.ts
+++ src/app/services/citas.service.ts
@@ -1,14 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CitasService {
 
+  route:string = 'citas/';
   constructor(private http:HttpClient) { }
 
   getAll(){
-    return this.http.get("http://localhost:3000/citas/");
+    return this.http.get(environment.endPointApi+this.route);
+  }
+  getById(id:string){
+    return this.http.get(environment.endPointApi+this.route+id);
   }
 }
