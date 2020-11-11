bdb76b93458ec59acfab5b2d5c90927d35ea0413
Alexander Graus <alegraus31@gmail.com>
2020-11-10 22:19:00 -0300

    cambio de la ruta con el endPointApi

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,17 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
+  routes:string = 'users/';
   constructor(private http:HttpClient) { }
 
   create(user){
-    return this.http.post("http://localhost:3000/users/registro",user);
+    return this.http.post(environment.endPointApi+this.routes+'registro',user);
   }
   login(user){
-    return this.http.post("http://localhost:3000/users/login",user);
+    return this.http.post(environment.endPointApi+this.routes+'login',user);
   }
 }
