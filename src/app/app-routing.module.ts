6c5a046e05c601ad00fca4663ba44e21a4ffdac2
Alexander Graus <alegraus31@gmail.com>
2020-11-21 22:23:08 -0300

    Correccion de la ruta home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { CitaComponent } from './Components/cita/cita.component';
 import { CitasComponent } from './Components/citas/citas.component';
 import { ContactoComponent } from './Pages/contacto/contacto.component';
+import { HomeComponent } from './Pages/home/home.component';
 import { LoginComponent } from './Pages/login/login.component';
 import { RegistroComponent } from './Pages/registro/registro.component';
 
 const routes: Routes = [
-  {path:"",component:CitasComponent},
+  {path:"",component:HomeComponent},
   {path:"cita/:id",component:CitaComponent},
   {path:"contacto",component:ContactoComponent},
   {path:'registro',component:RegistroComponent},
