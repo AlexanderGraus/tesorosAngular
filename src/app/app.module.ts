ef72729971265ec413c16ce571cd7b0963e4c2a6
Alexander Graus <alegraus31@gmail.com>
2020-11-21 21:48:53 -0300

    agregado imports de button material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import { RegistroComponent } from './registro/registro.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +35,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,MatButtonModule,MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
