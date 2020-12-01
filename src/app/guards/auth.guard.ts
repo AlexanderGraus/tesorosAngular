dc3777097ae943f3d3e3dd12b9d2e351049140e5
Alexander Graus <alegraus31@gmail.com>
2020-12-01 17:55:21 -0300

    crecion del auth.guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UsersService } from '../services/users.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardGuard implements CanActivate {
+  constructor(private usrServ:UsersService){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.usrServ.isAuthenticated();
+  }
+  
+}
