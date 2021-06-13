98862a3afdc503e62e9368b766fe14a0490f5d59
Alexander Graus <alegraus31@gmail.com>
2021-06-13 19:34:55 -0300

    version de server.js sin  htpps

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 /* Para desplegar en Heroku */
 
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(requireHTTPS);
 app.use(express.static('./dist/tesoros-angular'));
 
 app.get('/*', (req, res) =>
