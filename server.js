a5bb540fd0a6003ec5d988ab45bfa56c010752be
Alexander Graus <alegraus31@gmail.com>
2021-06-13 19:37:37 -0300

    version de server.js sin  htpps

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/tesoros-angular'));
+app.use(express.static('./dist/tesorosAngular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/tesoros-angular/'}),
+    res.sendFile('index.html', {root: 'dist/tesorosAngular/'}),
 );
 
 app.listen(process.env.PORT || 8080);
